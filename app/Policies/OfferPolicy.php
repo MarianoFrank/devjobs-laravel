eb37d427a546a59dc6bfe6cdf7d5b2ea8d475f70
Mariano Frank <marianofrank@hotmail.com>
2024-05-20 10:59:11 -0300

    first-commit

---
+++ app/Policies/OfferPolicy.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Offer;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\Auth;
+
+class OfferPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Offer $offer): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(): bool
+    {
+        return  Auth::user()->role === 2; //only recruite can create offers
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Offer $offer): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Offer $offer): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Offer $offer): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Offer $offer): bool
+    {
+        //
+    }
+}
---
+++ resources/views/auth/forgot-password.blade.php
@@ -0,0 +1,37 @@
+<x-guest-layout>
+    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+    </div>
+
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+
+    <form method="POST" action="{{ route('password.email') }}">
+        @csrf
+
+        <!-- Email Address -->
+        <div>
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            <x-primary-button>
+                {{ __('Email Password Reset Link') }}
+            </x-primary-button>
+        </div>
+
+        <div class="mt-5 flex justify-between">
+            <x-link href="{{ route('register') }}">
+                {{ __('Register an account') }}
+            </x-link>
+    
+            <x-link href="{{ route('login') }}">
+                {{ __('Already registered?') }}
+            </x-link>
+        </div>
+       
+
+    </form>
+</x-guest-layout>
